2f872028c7f1c2c7119d86a79a1664c5badb1306
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2025-09-02 14:34:01 -0600

    Project Created

---
+++ GamePicks/GamePicks/GamePicksApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamePicksApp.swift
+//  GamePicks
+//
+//  Created by Rodrigo Llaguno on 02/09/25.
+//
+
+import SwiftUI
+
+@main
+struct GamePicksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
